58540f41817fa3638b84738d31d4dd51ae02670c
pujiarahman <pujiarahman@gmail.com>
2019-01-23 12:47:09 +0700

    While Loops

---
+++ Bab4.py
@@ -0,0 +1,14 @@
+kondisi = 1
+while kondisi < 10:
+    print(kondisi)
+    kondisi += 1
+
+
+# Loops Tak Terhingga 
+
+while kondisi < 10:
+    print('Benar')
+
+while True:
+    print("Benar Sekali")
+    
