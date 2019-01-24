3d9ee4a01aaaa521fa09fca296d867b4002d2fe6
pujiarahman <pujiarahman@gmail.com>
2019-01-24 08:58:50 +0700

    Function Parameters

---
+++ Bab10.py
@@ -0,0 +1,15 @@
+def tambah(a,b,c,d):
+    x = a+b+c+d
+    print(x)
+
+tambah(1,2,3,4)
+
+
+def mahasiswa(nama,umur,semester,hp):
+    print('Nama:\t\t',nama)
+    print('Umur:\t\t',umur)
+    print('Semester:\t',semester)
+    print('Hp:\t\t',hp)
+
+mahasiswa('sidik','20','3','0821111')
+mahasiswa(nama='sidik',umur=20,semester=3,hp='021222290')
