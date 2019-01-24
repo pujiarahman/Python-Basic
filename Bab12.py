9e3354154220f2ee49dd5b8afe067f2c578dd75b
pujiarahman <pujiarahman@gmail.com>
2019-01-24 12:38:19 +0700

    Common Python Errors

---
+++ Bab12.py
@@ -0,0 +1,27 @@
+# NameError
+'''
+contoh = 'Data'
+print(conto)
+'''
+
+
+#Expected an Indented Block
+'''
+def contoh():
+def contoh1():
+    print('data')
+'''
+
+#Unexpected Indent 
+'''
+def contoh():
+    print('data')
+        print('data')
+print('data')
+'''
+
+#Unexpected EOF 
+'''
+x = [1,2,3,4,5
+def contoh():
+''' 
