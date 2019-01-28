39312e41500c27b863ee36850780dc749916a713
pujiarahman <pujiarahman@gmail.com>
2019-01-28 12:49:40 +0700

    Classes

---
+++ Bab16.py
@@ -0,0 +1,19 @@
+class berhitung:
+    def tambah(a,b):
+        x = a+b
+        print(x)
+    def kurang(a,b):
+        x = a-b
+        print(x)
+    def bagi(a,b):
+        x = a/b
+        print(x)
+    def kali(a,b):
+        x = a * b
+        print(x)
+
+berhitung.tambah(2,2)
+berhitung.kurang(2,2)
+berhitung.bagi(2,2)
+berhitung.kali(2,2)
+    
