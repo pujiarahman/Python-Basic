72bf3a0058101ddf9a947ffd1ba53d86d273fada
pujiarahman <pujiarahman@gmail.com>
2019-01-29 09:25:09 +0700

    Import Syntax

---
+++ Bab18.py
@@ -0,0 +1,13 @@
+# import statistics
+# import statistics as s
+# from statistics import mean,stdev
+# from statistics import *
+from statistics import mean as m, stdev as s
+
+x = [1,2,3,4,5,6,7,8,9,9]
+# print(statistics.mean(x))
+# print(s.mean(x))
+# print(mean(x))
+# print(stdev(x))
+print(m(x))
+print(s(x))
