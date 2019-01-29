3039935afcdd0507555a88a306952e129eb6ad88
pujiarahman <pujiarahman@gmail.com>
2019-01-29 08:41:56 +0700

    Input and Statistics

---
+++ Bab17.py
@@ -0,0 +1,22 @@
+#Contoh Input
+nama = input('Masukan Nama Anda ? ')
+print('helo', nama)
+
+#Contoh Statistic
+import statistics
+
+x = [1,2,3,4,5,6,7,8,9,9]
+a = statistics.mean(x)
+print(a)
+
+b = statistics.median(x)
+print(b)
+
+c = statistics.mode(x)
+print(c)
+
+d = statistics.stdev(x)
+print(d)
+
+e = statistics.variance(x)
+print(e)
