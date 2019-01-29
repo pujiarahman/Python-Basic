300ff0ddc6dfaa6119a21d9990cc751aaf587c7b
pujiarahman <pujiarahman@gmail.com>
2019-01-29 15:06:34 +0700

    Dictionaries

---
+++ Bab22.py
@@ -0,0 +1,18 @@
+x = {'Jaya':90,'Deni':80,'Gofar':80}
+print(x)
+
+x['Jaya']=100
+print(x)
+x['Deni']=100
+print(x)
+
+del x['Deni']
+
+print(x)
+
+s = {'Jason':[80,90,100],'Bondan':[90,80,80]}
+print(s)
+
+s['Jason'].append(90)
+print(s)
+
