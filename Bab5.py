7d9c4f5ddc926b5610d4a66496b012e16ab69426
pujiarahman <pujiarahman@gmail.com>
2019-12-14 15:48:07 +0700

    For Loops

---
+++ Bab5.py
@@ -0,0 +1,10 @@
+i = [1,2,3,4,5,6,7,8,9,10]
+
+for x in i:
+    print(x)
+
+for a in range(1,10):
+    print(a)
+
+    
+ #https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
